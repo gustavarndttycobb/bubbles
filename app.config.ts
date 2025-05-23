068d9d47b5b1f964e4ba774af15b2b5ee866915b
Maurício Alexandre Barroso <dev.mauricioalezandre@gmail.com>
2025-05-23 08:14:09 -0300

    apply configs to use EAS and deploy dev app

--- app.config.ts
+++ app.config.ts
@@ -6,8 +6,41 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: "bubbles",
   slug: "bubbles",
   version: "1.0.0",
+  orientation: "portrait",
+  icon: "./src/assets/images/icon.png",
+  scheme: "bubbles",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./src/assets/images/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.bubbles.app",
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./src/assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+    edgeToEdgeEnabled: true,
+    package: "com.bubbles.app",
+  },
+  web: {
+    favicon: "./src/assets/images/favicon.png",
+  },
+  owner: "devmauricioab",
   extra: {
-    apiUrl: process.env.API_URL,
-    mocksEnabled: process.env.MOCKS_ENABLED === 'true',
+    apiUrl: process.env.EXPO_PUBLIC_API_URL,
+    mocksEnabled: process.env.EXPO_PUBLIC_MOCKS_ENABLED === 'true',
+    router: {},
+    eas: {
+      projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+    },
   },
 });
